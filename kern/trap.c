5f03ad5ebff661800d00208781975d2c188855af
SantiAlme <santi.alme@gmail.com>
2018-11-08 15:11:04 -0300

    Update trap.c

--- kern/trap.c
+++ kern/trap.c
@@ -87,6 +87,8 @@ void simd_floating_point_exception();
 
 void syscall_trap();
 
+void irq_timer();
+
 void
 trap_init(void)
 {
@@ -115,6 +117,8 @@ trap_init(void)
 	SETGATE(idt[T_SIMDERR], 0, GD_KT, (&simd_floating_point_exception), 3)
 
 	SETGATE(idt[T_SYSCALL], 1, GD_KT, (&syscall_trap), 3)
+		
+	SETGATE(idt[IRQ_OFFSET+IRQ_TIMER], 0, GD_KT, (&irq_timer), 3)
 
 	// Per-CPU setup
 	trap_init_percpu();
