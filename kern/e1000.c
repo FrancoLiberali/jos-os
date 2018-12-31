8a0f8fdef4b232c5e994a5f46f35f79260d1652a
FrancoLiberali <franco.liberali@gmail.com>
2018-12-31 19:25:41 -0300

    exercise 4

--- kern/e1000.c
+++ kern/e1000.c
@@ -1,8 +1,13 @@
 #include <kern/e1000.h>
+#include <kern/pmap.h>
 
 // LAB 6: Your driver code here
 
+volatile void *e1000;
+
 int e1000_attachfn (struct pci_func *pcif){
     pci_func_enable(pcif);
+    e1000 = mmio_map_region(pcif->reg_base[0], pcif->reg_size[0]);
+    cprintf("E1000 Service status register %0x\n", *((uint32_t*)(e1000 + E1000_STATUS)));
     return 0;
 }
--- kern/e1000.h
+++ kern/e1000.h
@@ -6,6 +6,20 @@
 #define E1000_VENDOR_ID 0x8086
 #define E1000_DEVICE_ID 0x100E
 
+/* Register Set. (82543, 82544)
+ *
+ * Registers are defined to be 32 bits and  should be accessed as 32 bit values.
+ * These registers are physically located on the NIC, but are mapped into the
+ * host memory address space.
+ *
+ * RW - register is both readable and writable
+ * RO - register is read only
+ * WO - register is write only
+ * R/clr - register is read only and is cleared when read
+ * A - register array
+ */
+#define E1000_STATUS   0x00008  /* Device Status - RO */
+
 int e1000_attachfn (struct pci_func *pcif);
 
 #endif  // JOS_KERN_E1000_H
