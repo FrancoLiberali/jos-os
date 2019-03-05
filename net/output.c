968dcbb685bd227da6effcf6c13845b399627728
FrancoLiberali <franco.liberali@gmail.com>
2019-03-05 15:48:22 -0300

    correcciones v2

--- net/output.c
+++ net/output.c
@@ -9,11 +9,11 @@ output(envid_t ns_envid)
 	// LAB 6: Your code here:
 	// 	- read a packet from the network server
 	//	- send the packet to the device driver
-	static union Nsipc nsipcoutbuf __attribute__((aligned(PGSIZE)));
+	struct jif_pkt *nsipcoutbuf = (struct jif_pkt *) REQVA;
 	envid_t from_env_store;
 	while (true) {
 		int32_t value =
-		        ipc_recv(&from_env_store, (void *) &nsipcoutbuf, NULL);
+		        ipc_recv(&from_env_store, (void *) nsipcoutbuf, NULL);
 		if (value < 0) {
 			panic("ipc_send: failed to send");
 		}
@@ -22,8 +22,7 @@ output(envid_t ns_envid)
 			int error = 1;
 			while (error) {
 				error = sys_e1000_try_transmit(
-				        nsipcoutbuf.pkt.jp_data,
-				        nsipcoutbuf.pkt.jp_len);
+				        nsipcoutbuf->jp_data, nsipcoutbuf->jp_len);
 				// if transmit queue full
 				if (error) {
 					sys_yield();
